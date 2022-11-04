bc1bf0687db0c0e00351b9bc7870cab426872bfc
dorababu18 <111499201+dorababu18@users.noreply.github.com>
2022-11-04 11:25:30 +0530

    import math as m n=int(input()) m=int(input()) for i in range(n,m):     if i>1:         for j in range(2,int(i**0.5)+1):             if i%j==0:                 break         else:             print(i)

--- Generate_Prime_Numbers_In_A_Interval_.py
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -1,11 +1,10 @@
-import math
-a=int(input())
-b=int(input())
-for i in range(a,b):
+import math as m
+n=int(input())
+m=int(input())
+for i in range(n,m):
     if i>1:
-        for j in range(2,int(math.sqrt(i)+1)):
+        for j in range(2,int(i**0.5)+1):
             if i%j==0:
                 break
         else:
             print(i)
-        
